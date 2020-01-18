d66cee19d7c4caa7a43449a17a587d17169114e6
John Johnson-Rodgers <john@johnthe.dev>
2020-01-18 16:43:21 -0700

    began style sheet

--- index.php
+++ index.php
@@ -8,9 +8,31 @@
 	<!--link to css-->
 	<link rel="stylesheet" href="style.css">
 	<!-- title-->
-	<title>Literary Mutilator</title>
+	<title>John's Portfolio Page</title>
 </head>
+<?php
+if(array_key_exists('button1', $_POST)) (
+?>
 <body>
+<!--Header section with quick links to various sections and a resume downloader-->
+<div id="headerBar">
 
+</div>
+<!--banner section with who I am and a picture to capture readers attention and convey purpose-->
+<div id="banner">
 
+</div>
+<!--Pages selected to be examples of what sort of work I have done in the past. Try to limit to ten or so-->
+<div id="portfolio">
+
+</div>
+<!--contact form for people to reach out to me. Make it clear that I am not looking for a job, just to help.-->
+<div id="contactForm">
+
+</div>
+<!--Here I will link to social media, github, etc. Maybe include a quote. Not sure if I want this to be sticky or not.-->
+<div id="footer">
+
+</div>
 </body>
+</html>
